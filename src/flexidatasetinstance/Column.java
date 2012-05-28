f88585fd7dfc4617a93d7bb58f48786aca4c60bb
Gourab Mitra <gourab5139014@gmail.com>
2012-05-28 21:55:05 +0530

    progress on parsing dataSet

--- src/flexidatasetinstance/Column.java
+++ src/flexidatasetinstance/Column.java
@@ -10,9 +10,20 @@ package flexidatasetinstance;
  * @author Gourab
  */
 public class Column {
-    private String name;
+    //private String name;
     private int index;
     private DataType type;
+    private boolean process;
+
+    public Column(int index, boolean process) {
+        this.index = index;
+        this.process = process;
+    }
+
+    public void setType(DataType type) {
+        this.type = type;
+    }
+
 
     public Object convert( String data ) {
         if( type == DataType.NUMERIC ) {
--- src/flexidatasetinstance/FlexiInstance.java
+++ src/flexidatasetinstance/FlexiInstance.java
@@ -7,9 +7,10 @@ package flexidatasetinstance;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.BitSet;
 import java.util.List;
 
 /**
@@ -18,7 +19,7 @@ import java.util.List;
  */
 public class FlexiInstance {
     private String filename;
-    private BitSet toProcess; //decide whether to process a column or not
+    private List<Integer> toProcess; //decide whether to process a column or not
     private String bitSetFilename; //corresponding .bitset file containing which columns to process
 
     private List<Column> columns ;
@@ -29,18 +30,78 @@ public class FlexiInstance {
         this.bitSetFilename = "."+File.separator+"datasets"+File.separator+filename+".bitSet";
         columns = new ArrayList<Column>();
         rows = new ArrayList<Row>();
+        toProcess = new ArrayList<Integer>();
 
     }
-    public void parseFile() throws Exception
+    private void parseFile()
     {
-        //parse File to assign Column data as CATEGORICAL, NUMERIC
-        BufferedReader Reader = new BufferedReader(new FileReader(this.filename));
+        try {
+            //parse File to assign Column data as CATEGORICAL, NUMERIC
+            BufferedReader reader = new BufferedReader(new FileReader(this.filename));
+            String datarow = reader.readLine();
+            if (datarow == null) {
+                reader.close();
+                System.err.println("DataSet File Empty! ");
+                return;
+            }
+            String dataArray[] = datarow.split(",");
+            for(int i=0;i<dataArray.length;i++){ //initialize columns with index and process permission values
+                columns.add(new Column(i,(toProcess.get(i)==1?true:false)));
+            }
+            //now determine type using the one row read in dataArray
+            Column temp;
+            for (int i=0;i<dataArray.length;i++) {
+                temp=columns.get(i);
+                try {
+                    //try Integer.parseInt()
+                    Integer.parseInt(dataArray[i]);
+                    temp.setType(DataType.NUMERIC);
 
+                } catch (NumberFormatException nfe) {
+                    //set as catagorical
+                    temp.setType(DataType.CATEGORICAL);
+                }
+                
+            }
+            //now populate all rows using the above obtained information
+            reader.close();
+        } catch (FileNotFoundException fnfe) {
+            System.err.println("DataSet FileNotFound! " + fnfe.getLocalizedMessage());
+
+        } catch (IOException ioe) {
+            System.err.println("Inside parseFile() " + ioe.getLocalizedMessage());
+        }
+    }
+    private void readColumnPermission()
+    {
+        try{
+        BufferedReader reader = new BufferedReader(new FileReader(this.bitSetFilename));
+        String bitrow = reader.readLine();
+        System.err.println("Read permission : "+bitrow+" from "+bitSetFilename);
+        for(int i=0;i<bitrow.length();i++)
+        {
+            toProcess.add(i, Integer.parseInt(bitrow.substring(i, i+1)));
+        }
+        reader.close();
+        }catch(FileNotFoundException fnfe) {
+            System.err.println("Permissions preferences file <dataSetName>.bitSet not Found! Reverting to default permission preferences.");
+            /*
+             * Code to write all 1s in toProcess permitting processing of all attributes except the last one (class column)
+             */
+
+        }
+        catch(IOException ioe) { System.err.println("Inside readColumnPermission() " + ioe.getLocalizedMessage());}
     }
     public void run()
     {
         try{
             //all calls from here
+            /*
+             * Order of calls
+             * 1. parse file to read and configure columns
+             * 2. readColumnPermission on created columns
+             */
+            readColumnPermission();
         }catch(Exception e)
         {
             System.err.println("Inside Run() : "+e.getLocalizedMessage());
